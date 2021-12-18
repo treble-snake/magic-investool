04efc974cf43bee1c3bfacc39dfce8350acd6b14
treble-snake <treble.snake@gmail.com>
2021-12-18 10:42:58 +0200

    feat(web-ui): improve history editing
    Use smart history operations instead of storage directly.
    Add global error boundary.

--- web-ui/pages/api/history/[id].ts
+++ web-ui/pages/api/history/[id].ts
@@ -1,5 +1,5 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
-import {defaultContext} from '@investool/engine';
+import {defaultContext, historyOperations} from '@investool/engine';
 
 type Body = {
   price: number,
@@ -12,17 +12,17 @@ export default async function handler(
   res: NextApiResponse<void>
 ) {
   const id = req.query.id as string;
+  const ops = historyOperations(defaultContext());
+
   if (req.method === 'DELETE') {
-    await defaultContext().historyStorage.deleteRecord(id);
+    await ops.delete(id);
 
     res.status(204).send();
   }
 
   if (req.method === 'PUT') {
-    const {price, qty, date} = req.body as Body;
-    await defaultContext().historyStorage.updateRecord(
-      id, {price, qty, date}
-    )
+    const {price, qty} = req.body as Body;
+    await ops.updateOne(id, {price, qty});
     return res.status(204).send();
   }
 
