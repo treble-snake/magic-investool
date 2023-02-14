cd11afc2c6a410a663dc62069612effff6b9788b
treble-snake <treble.snake@gmail.com>
2023-02-14 22:23:37 +0200

    build(engine): upgrade dependencies

--- engine/src/portfoio/findOverdueItems.ts
+++ engine/src/portfoio/findOverdueItems.ts
@@ -4,8 +4,9 @@ import {filter, pipe, prop, sortBy} from 'ramda';
 
 export const findOverdueItems = (companies: PortfolioCompany[], date = new Date()) => {
   const checkDate = endOfMonth(subYears(date, 1));
-  return pipe(
-    filter((it: PortfolioCompany) => isBefore(new Date(it.purchaseDate), checkDate)),
-    sortBy(prop('purchaseDate'))
-  )(companies);
+  const overdue = filter(
+    (it: PortfolioCompany) => isBefore(new Date(it.purchaseDate), checkDate),
+    companies
+  );
+  return sortBy(prop('purchaseDate'), overdue);
 };
