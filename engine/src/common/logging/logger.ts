3d7229dbbfddbe47c23161c731274180a640bcff
treble-snake <treble.snake@gmail.com>
2023-02-18 14:37:56 +0200

    chore: add winston logger

--- engine/src/common/logging/logger.ts
+++ engine/src/common/logging/logger.ts
@@ -1,1 +1,12 @@
-export const logger = console;
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.cli(),
+  transports: [
+    new winston.transports.Console({
+      handleExceptions: true,
+      handleRejections: true
+    })
+  ]
+});
--- engine/test/jest.setup.js
+++ engine/test/jest.setup.js
@@ -1,2 +1,4 @@
 // TODO: should not be necessary
 process.env.STORAGE_DIR = './test/fixtures';
+
+process.env.LOG_LEVEL = 'warn';
