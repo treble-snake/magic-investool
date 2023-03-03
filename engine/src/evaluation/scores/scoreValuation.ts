1818b444857790bd24f1aa8cbe936b3ee1ccae76
treble-snake <treble.snake@gmail.com>
2023-03-03 18:04:19 +0200

    feat(engine): fix some scoring

--- engine/src/evaluation/scores/scoreValuation.ts
+++ engine/src/evaluation/scores/scoreValuation.ts
@@ -1,14 +1,12 @@
-import {ValuationData, ValuationType} from '../../common/types/ranking.types';
+import {CompanyStock} from '../../common/types/companies.types';
 
-export const scoreValuation = (data: ValuationData) => {
-  switch (data.type) {
-    case ValuationType.NearFair:
-      return 0;
-    case ValuationType.Undervalued:
-      return data.percentage;
-    case ValuationType.Overvalued:
-      return -1 * data.percentage;
-    default:
-      return -100;
+// TODO: rename and rework this
+export const scoreValuation = (company: CompanyStock) => {
+  const price = company.price || 0;
+  const target = company.overview.analystTargetPrice || 0;
+  if (price === 0 || target === 0) {
+    return 0;
   }
+
+  return target / price;
 };
